e74548f69b85ed5e2ddb4b652c64cec5184a2ec4
RafaelaMG <rafaela.m.guerra@gmail.com>
2018-02-05 13:14:05 +0100

    removes config info

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  host: 'http://localhost:8080', // e.g. local docker Cockpit.
-  webSocket: 'wss://cockpit-realtime.now.sh:4000',
-  accessToken: '5e7be6f3bba4c820bb79f11c430a69',
+  host: '<HOST>', // e.g. local docker Cockpit.
+  webSocket: '<WEBSOCKET',
+  accessToken: 'ACCESS_TOKEN>',
 };
